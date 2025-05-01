7573c4dfbcaebc9f54695a2b3a9bb4a48aea6345
apk02211 <apk02211@gmail.com>
2025-05-01 20:44:31 +0530

    Update Assign_4__Qno-6.cpp

--- Assignment - 4/Assign_4__Qno-6.cpp
+++ Assignment - 4/Assign_4__Qno-6.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class AddAmount {
+    int amount;
+
+public:
+    AddAmount() {
+        amount = 50;
+    }
+
+    AddAmount(int add) {
+        amount = 50 + add;
+    }
+
+    void displayAmount() {
+        cout << "Final amount: " << amount << endl;
+    }
+};
+
+int main() {
+    AddAmount obj1;
+    obj1.displayAmount();
+
+    AddAmount obj2(30);
+    obj2.displayAmount();
+
+    return 0;
+}
 
